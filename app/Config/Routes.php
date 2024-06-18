edf65b96c6e7e1ddf31db37a642c68688e7c46eb
Yael-Dev <aldoyael.dev@gmail.com>
2024-06-17 18:08:33 -0600

    implentacion/rutas login y dashborad

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,6 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('dashboard', 'Home::dashboard');
+
+$routes->post('login', 'AuthController::login');
