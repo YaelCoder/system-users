9c600257e794db97cf7d619fdec73e974eac4be3
Yael-Dev <aldoyael.dev@gmail.com>
2024-06-17 18:13:31 -0600

    implementacion/vista principal de layout

---
+++ app/Views/telat/dashboard/index.php
@@ -0,0 +1,47 @@
+<?= $this->extend('telat/layouts/template') ?>
+
+<?= $this->section('title') ?>Dashboard<?= $this->endSection() ?>
+
+<?= $this->section('styles') ?>
+<style>
+    i {
+        font-size: 50px;
+        color: gray;
+    }
+</style>
+<?= $this->endSection() ?>
+
+<?= $this->section('content') ?>
+<div class="row">
+    <div class="col-sm-6 mb-3">
+        <div class="card text-center">
+            <h5 class="card-header">Listado de usuarios</h5>
+            <div class="card-body">
+                <i class="bi bi-person-lines-fill"></i>
+                <p class="card-text">Visualiza toda la lista de usuarios</p>
+                <a href="#" class="btn btn-primary">Ver listado</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-6 mb-3">
+        <div class="card text-center">
+            <h5 class="card-header">Agregar usuarios</h5>
+            <div class="card-body">
+                <i class="bi bi-person-fill-add"></i>
+                <p class="card-text">Agrega un nuevo usuario</p>
+                <a href="#" class="btn btn-primary">Agregar</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-6 mb-3">
+        <div class="card text-center">
+            <h5 class="card-header">Reportes de usuarios</h5>
+            <div class="card-body">
+                <i class="bi bi-person-fill-down"></i>
+                <p class="card-text">Aqui puedes decargar la lista de reportes de usuarios.</p>
+                <a href="#" class="btn btn-primary">Decargar reporte</a>
+            </div>
+        </div>
+    </div>
+</div>
+<?= $this->endSection() ?>
